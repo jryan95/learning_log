72eff3d1d6abed7b03b72ba72fba40b2636b1bf5
Jon <jcryan00@gmail.com>
2020-07-29 00:58:16 -0500

    update end chapter 19

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -14,11 +14,13 @@
 # work on your own projects.
 
 from django.db import models
+from django.contrib.auth.models import User
 
 class Topic(models.Model):
     """ A topic the user is learning about. """
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """ Return a string representation of the model. """
